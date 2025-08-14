d27e1270bd3cf6bb1288c229e2496a8b54dc7490
jxtinnracines <aisat.justineracines246316@gmail.com>
2025-08-14 05:06:37 -0700

    add tailwind css and configuring shadcn ( not completed )

--- portfolio-2025/src/pages/home.tsx
+++ portfolio-2025/src/pages/home.tsx
@@ -1,6 +1,22 @@
+import LightRays from '../components/ReactBits/LightRays';
+import BlurText from "../components/ReactBits/BlurText";
 
 export default function Home() {
-  return (
-    <div>home</div>
-  )
+
+    const handleAnimationComplete = () => {
+      console.log('Animation completed!');
+    };
+
+    return (
+        <div className='w-full h-[600px] relative bg-black text-white' >
+        <BlurText
+            text="Jonel Teano"
+            delay={1000}
+            animateBy="words"
+            direction="top"
+            onAnimationComplete={handleAnimationComplete}
+            className="text-9xl mb-8"
+        />
+        </div>
+    )
 }
